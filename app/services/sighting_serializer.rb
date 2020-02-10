aecfffc78fa7e276f16e754ab42b1da799ef92c1
Emery Tate <tateejt@gmail.com>
2020-02-10 00:37:13 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -3,5 +3,9 @@ class SightingSerializer
   def initialize(sighting_object)
     @sighting = sighting_object
   end
-
+  
+  def to_serialized_json
+    @sighting.to_json(:include => {
+      
+    })
 end
