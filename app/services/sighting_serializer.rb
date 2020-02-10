ef60b7dbe3b6ab480b6c522b9a6dd7f583adbe48
Emery Tate <tateejt@gmail.com>
2020-02-10 00:37:34 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,7 +5,8 @@ class SightingSerializer
   end
   
   def to_serialized_json
-    @sighting.to_json(:include => {
-      
-    })
+  @sighting.to_json(:include => {
+    :bird => {:only => [:name, :species]},
+    :location => {:only => [:latitude, :longitude]}
+  }, :except => [:updated_at])
 end
