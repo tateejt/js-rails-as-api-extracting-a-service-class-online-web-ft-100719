bdcf42e98f3a17ad778d8b49da264ba69c37e108
Emery Tate <tateejt@gmail.com>
2020-02-10 00:50:04 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,10 +1,7 @@
 class SightingsController < ApplicationController
   def index
     sightings = Sighting.all
-    render json: sightings.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    render json: SightingSerializer.new(sightings).to_serialized_json
   end
  
   def show
